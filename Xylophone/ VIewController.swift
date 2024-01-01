0c62125be245f088d913e83cade7191967405d8b
Haider <haider@192.168.10.2>
2024-01-01 20:08:32 +0500

    Issue fixed now working as intentioned

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -11,26 +11,29 @@ import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var avPlayer : AVAudioPlayer!
+    let soundsArr = ["note1","note2","note3","note4","note5","note6","note7"]
+    var selectedSound = ""
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        print("Pressed \(sender.tag)")
-        var avPlayer = AVAudioPlayer()
-        
-        guard let url = Bundle.main.url(forResource: "Game", withExtension: "mp3")
-           else {
-              return
-           }
-           do {
-               avPlayer = try AVAudioPlayer(contentsOf: url)
-              avPlayer.play()
-           }
-           catch {
-           }
+        selectedSound = soundsArr[sender.tag-1]
+        print(selectedSound)
+        playNotes()
+       
+    }
+    
+    func playNotes(){
+        let url = Bundle.main.url(forResource: selectedSound, withExtension: "wav")
+                 
+                  do {
+                      avPlayer = try AVAudioPlayer(contentsOf: url!)
+                     avPlayer.play()
+                  }
+                  catch {
+                  }
     }
 }
